5a63ebfbecc52a7c7bb3028aca2bf59602b92c7a
Vlad Zayats <wallhalu@gmail.com>
2024-08-20 19:13:13 +0300

    Pieces models

--- src/models/pieces/Piece.ts
+++ src/models/pieces/Piece.ts
@@ -1,3 +1,37 @@
+import { Cell } from "../CellModel";
+import { Colors } from "../Colors";
+
+enum PiecesNames {
+    PIECE = "",
+    PAWN = "pawn",
+    ROCK = "rock",
+    KNIGHT = "knight",
+    BISHOP = "bishop",
+    QUEEN = "queen",
+    KING = "king"
+} 
+
 export class Piece {
-    
+    color: Colors;
+    logo: string | null;
+    cell: Cell;
+    name: PiecesNames;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.logo = null;
+        this.name = PiecesNames.PIECE;
+        this.cell.piece = this;
+        this.id = Math.random();
+    }
+
+    canMove(cell: Cell): boolean {
+        return true;
+    }
+
+    move(cell: Cell) {
+        
+    }
 }
